83e95bb2e100f1d5ca7f4d5d1e02423ca5e86f53
Roman Galeev <jamhedd@gmail.com>
2020-02-03 03:06:19 +0100

    Support sync up/down and fix exec

--- entry.sh
+++ entry.sh
@@ -3,4 +3,10 @@ if [ -z "$LOCATION" ]; then
 	echo "Please provide environment variables required"
 	exit 1
 fi
-rclone sync S3:$LOCATION
+
+if [ "$DOWNLOAD" ]
+then
+	rclone sync /mnt/private S3:$LOCATION
+else
+	rclone sync S3:$LOCATION /mnt/private
+fi
